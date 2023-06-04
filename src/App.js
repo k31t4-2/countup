9ff8c189a25c26165c8a24e15b6c8996960f5579
KEITA <ksekimotonz@gmail.com>
2023-06-04 20:05:22 +0900

    done yesterday

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const countUp = () => {
+    setCount(prevCount => prevCount + 1)
+  }
+
+  const countDown = () => {
+    setCount(count => count - 1)
+  }
+
+  const reset = () => {
+    setCount(0)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>カウントアプリを作る</h1>
+      <p>{count}</p>
+      <button onClick={countUp}>Increment</button>
+      <button onClick={countDown}>Decrement</button>
+      <button onClick={reset}>reset</button>
     </div>
   );
 }
